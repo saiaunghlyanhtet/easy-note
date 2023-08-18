b4f7b8e7aa86210c13902b678e84f9c1e8347d23
Sai Aung Hlyan Htet <saiaunghlyanhtet21303@gmail.com>
2023-08-19 00:03:51 +0630

    Added Billing Api and unit tests

--- stacks/ApiStack.ts
+++ stacks/ApiStack.ts
@@ -1,8 +1,13 @@
 import { Api, use, StackContext } from "sst/constructs";
 import { StorageStack } from "./StorageStack";
 
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY as string;
+
+
+
 export function ApiStack({ stack }: StackContext) {
   const { table } = use(StorageStack);
+  
 
   // Create the API
   const api = new Api(stack, "Api", {
@@ -10,6 +15,9 @@ export function ApiStack({ stack }: StackContext) {
       authorizer: "iam",
       function: {
         bind: [table],
+        environment: {
+          STRIPE_SECRET_KEY: stripeSecretKey,
+        },
       },
     },
     routes: {
@@ -18,6 +26,7 @@ export function ApiStack({ stack }: StackContext) {
       "GET /notes": "packages/functions/src/list.main",
       "PUT /notes/{id}": "packages/functions/src/update.main",
       "DELETE /notes/{id}": "packages/functions/src/delete.main",
+      "POST /billing": "packages/functions/src/billing.main",
     },
   });
 
